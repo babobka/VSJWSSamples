871aca3b5a222d4f0a9004ac5d98566c6684f409
Долгополов Антон <dolgopolov.a@Content.local>
2016-08-18 16:21:26 +0300

    Little bug fixed

--- src/ru/babobka/vsjws/Main.java
+++ src/ru/babobka/vsjws/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import ru.babobka.vsjws.model.WebServer;
 import ru.babobka.vsjws.webcontroller.CookieTestController;
-import ru.babobka.vsjws.webcontroller.EncodingTestController;
 import ru.babobka.vsjws.webcontroller.HeavyRequestController;
 import ru.babobka.vsjws.webcontroller.InternalErrorController;
 import ru.babobka.vsjws.webcontroller.JsonTestController;
@@ -28,8 +27,7 @@ public class Main {
 				SESSION_TIMEOUT_SECS, WEB_CONTENT_FOLDER, "rest_log", null);	
 		//Adding controllers for a specified URLs			
 		webServer.addController("/json", new JsonTestController());
-		webServer.addController("/xml", new XmlTestController());
-		webServer.addController("/encoding", new EncodingTestController());
+		webServer.addController("/xml", new XmlTestController());		
 		webServer.addController("/heavy", new HeavyRequestController());
 		webServer.addController("/error", new InternalErrorController());
 		webServer.addController("/session", new SessionTestController());
