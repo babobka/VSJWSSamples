56f825d7b302ca7c1f9858ac85c4af7472a0afe5
Долгополов Антон <dolgopolov.a@Content.local>
2016-08-19 10:59:13 +0300

    Little fix

--- src/ru/babobka/vsjws/Main.java
+++ src/ru/babobka/vsjws/Main.java
@@ -2,7 +2,7 @@ package ru.babobka.vsjws;
 
 import java.io.IOException;
 
-import ru.babobka.vsjws.model.WebServer;
+import ru.babobka.vsjws.webserver.WebServer;
 import ru.babobka.vsjws.webcontroller.CookieTestController;
 import ru.babobka.vsjws.webcontroller.HeavyRequestController;
 import ru.babobka.vsjws.webcontroller.InternalErrorController;
@@ -19,15 +19,18 @@ public class Main {
 
 	private static final int SESSION_TIMEOUT_SECS = 15 * 60;
 
-	public static final String WEB_CONTENT_FOLDER = "web-content";
+	private static final String WEB_CONTENT_FOLDER = "web-content";
 
-	public static void main(String[] args) throws IOException
-			 {
-		WebServer webServer = new WebServer("rest server", PORT, false,
-				SESSION_TIMEOUT_SECS, WEB_CONTENT_FOLDER, "rest_log", null);	
-		//Adding controllers for a specified URLs			
+	private static final String SERVER_NAME = "Sample server";
+
+	private static final String LOG_FOLDER = "server_log";
+
+	public static void main(String[] args) throws IOException {
+		WebServer webServer = new WebServer(SERVER_NAME, PORT,
+				SESSION_TIMEOUT_SECS, WEB_CONTENT_FOLDER, LOG_FOLDER, null);
+		// Adding controllers for a specified URLs
 		webServer.addController("/json", new JsonTestController());
-		webServer.addController("/xml", new XmlTestController());		
+		webServer.addController("/xml", new XmlTestController());
 		webServer.addController("/heavy", new HeavyRequestController());
 		webServer.addController("/error", new InternalErrorController());
 		webServer.addController("/session", new SessionTestController());
